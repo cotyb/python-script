90b579c82bcfba97048c89a096e4e59127c9f6e0
cotyb <xieleicotyb@gmail.com>
2015-07-11 11:16:07 +0800

    print functions
    when running the python codes, the console will print the functions are
    running

---
+++ printfun.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+name = open("xxx","r")
+new_data = open("yyy","r+")
+line = name.readline()
+while line:
+    new_data.write(line)
+    if line[0:3] == 'def':
+        line = line[0:-1]
+        new_data.write("print " + "\"" + "%s"  %line + "\"\n")
+    line = name.readline()
+	
+name.close()
+new_data.close()
