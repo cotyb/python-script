db5b1d58c01584c2000b7cbf82eef4a5a6209796
cotyb <xieleicotyb@gmail.com>
2015-07-11 11:32:53 +0800

    print functions
    when running the python codes, the console will print the functions are
    running, and this script can only been run on Windows correctly

--- printfun.py
+++ printfun.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-name = open("xxx","r")
-new_data = open("yyy","r+")
+name = open("xx.x","r")
+new_data = open("yy.y","r+")
 line = name.readline()
 while line:
     new_data.write(line)
     if line[0:3] == 'def':
         line = line[0:-1]
-        new_data.write("print " + "\"" + "%s"  %line + "\"\n")
+        new_data.write("    print " + "\"" + "%s"  %line + "\"\n")
     line = name.readline()
 	
 name.close()
