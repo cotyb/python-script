46151dd4f7d290424fbae27083a44bcd8e77a3d8
cotyb <xieleicotyb@gmail.com>
2015-08-18 16:49:08 +0800

    upload_ssh
    upload files

---
+++ ssh_python/upload_ssh.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import paramiko
+t = paramiko.Transport(("192.168.29.188",22))
+t.connect(username="cotyb",password="111111")
+sftp = paramiko.SFTPClient.from_transport(t)
+remotepath = '/home/cotyb/chen.txt'
+localpath = '/home/cotyb/chen.txt'
+sftp.put(localpath,remotepath)
+t.close()
